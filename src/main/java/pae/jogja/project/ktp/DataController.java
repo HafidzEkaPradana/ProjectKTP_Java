448a7230ca3577ea373840b190d12654bef45ec5
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-04-20 00:19:34 +0700

    insert data done but not insert foto

--- src/main/java/pae/jogja/project/ktp/DataController.java
+++ src/main/java/pae/jogja/project/ktp/DataController.java
@@ -9,17 +9,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -27,43 +24,44 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Controller
 public class DataController {
-    
+
     DataJpaController datactrl = new DataJpaController();
+    Data data = new Data();
     List<Data> newdata = new ArrayList<>();
-    
+
     @RequestMapping("/data")
     //@ResponseBody
-    public String getDataKTP(Model model){
-        
+    public String getDataKTP(Model model) {
+
         int record = datactrl.getDataCount();
-        
-        try{
+
+        try {
             newdata = datactrl.findDataEntities().subList(0, record);
+        } catch (Exception e) {
         }
-        catch(Exception e){}
         model.addAttribute("goData", newdata);
         model.addAttribute("record", record);
-        
+
         return "database";
 
     }
-    
+
     @RequestMapping("/create")
-    public String createData(){
-        
+    public String createData() {
+
         return "create";
     }
-    
-    @RequestMapping(value="/newdata", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String newData(@RequestParam("gambar")MultipartFile file, HttpServletRequest data) throws ParseException, Exception{
+
+    @RequestMapping("/newdata")
+    public String newData(HttpServletRequest data) throws ParseException, Exception {
         String result = "";
         Data datadat = new Data();
-        
+
         String idInput = data.getParameter("id");
         int iid = Integer.parseInt(idInput);
         String nonik = data.getParameter("noktp");
         String namaInput = data.getParameter("name");
-        String tanggal = data.getParameter("");
+        String tanggal = data.getParameter("tanggal");
         Date date = new SimpleDateFormat("yyyy-MM-dd").parse(tanggal);
         String jenis = data.getParameter("jenis");
         String alamatInput = data.getParameter("alamat");
@@ -71,14 +69,14 @@ public class DataController {
         String status = data.getParameter("status");
         String pekerjaan = data.getParameter("kerja");
         String warganegara = data.getParameter("warganegara");
-        String berlaku = data.getParameter("berlaku");
-        String filename = StringUtils.cleanPath(file.getOriginalFilename());
-        byte[] image = file.getBytes();
-        
-        
+        String berlaku = "Seumur Hidup";
+        //String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        //filename = filename.toLowerCase().replaceAll(" ", "-");
+        //byte[] image = file.getBytes();
+
         datadat.setId(iid);
-        datadat.setNama(namaInput);
         datadat.setNoktp(nonik);
+        datadat.setNama(namaInput);
         datadat.setTgllahir(date);
         datadat.setJeniskelamin(jenis);
         datadat.setAlamat(alamatInput);
@@ -87,19 +85,20 @@ public class DataController {
         datadat.setPekerjaan(pekerjaan);
         datadat.setKewarganegaraan(warganegara);
         datadat.setBerlakuhingga(berlaku);
-        datadat.setFoto(image);
-        
+        //datadat.setFoto(image);
+
         datactrl.create(datadat);
-        
+
         return "database";
     }
-    
+
     @RequestMapping("/edit")
-    public String editData(){
+    public String editData() {
         return "edit";
     }
+
     @RequestMapping("/delete")
-    public String deleteData(){
-        return "delete";
+    public String deleteData() {
+        return "database";
     }
 }
